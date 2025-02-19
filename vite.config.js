b66bfdebac34b6e4222dc9d841fdf507e076fca9
Jabin0214 <jabinchen0214@gmail.com>
2025-02-20 12:29:41 +1300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // 添加这一行
+  base: "/my-page/", // 这里要和你的仓库名一致
   plugins: [react()],
 })
